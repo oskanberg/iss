309a235f9af8204b65175071b36ec556d734e57a
Oliver Skanberg-Tippen <ost500@york.ac.uk>
2015-04-17 17:21:31 +0100

    replace old genetics plot with new

--- plot_meanGenetics.py
+++ plot_meanGenetics.py
@@ -1,118 +1,75 @@
 #!/usr/bin/python
 
-import sys, json
+import json, sys
+import matplotlib 
 import matplotlib.pyplot as plt
 
-
-YLIM = 100
-
+# read files
 f1_data = {}
 with open(sys.argv[1], 'r') as f:
 	f1_data = json.load(f)
 
 
 f2_data = {}
-if len(sys.argv) > 2:
-	with open(sys.argv[2], 'r') as f:
-		f2_data = json.load(f)
+with open(sys.argv[2], 'r') as f:
+	f2_data = json.load(f)
 
 
+# Parse into series
 attraction_same = []
 attraction_other = []
-
 orientation_same = []
 orientation_other = []
 for step in f1_data:
 	attraction_same.append(step['Attraction']['s'])
 	attraction_other.append(step['Attraction']['o'])
-
 	orientation_same.append(step['Orientation']['s'])
 	orientation_other.append(step['Orientation']['o'])
 
-plt.suptitle('Mean genetics', fontsize=15)
 
-print sys.argv
-if '-alt' in sys.argv:
-	plt.figure(1)
-	plt.subplot(221)
-	plt.title('same')
-	plt.plot(orientation_same, 'b', label="Orientation")
-	plt.plot(attraction_same, 'k', label="Attraction")
-	x1,x2,y1,y2 = plt.axis()
-	plt.axis((x1,x2,0,YLIM))
-	plt.legend(loc='best', prop={'size':8})
+# plot
+cols = ['Column {}'.format(col) for col in range(1, 3)]
+rows = ['Row {}'.format(row) for row in ['A', 'B']]
 
-	plt.subplot(222)
-	plt.title('other')
-	plt.plot(orientation_other, 'b', label="Orientation")
-	plt.plot(attraction_other, 'k', label="Attraction") 
-	x1,x2,y1,y2 = plt.axis()
-	plt.axis((x1,x2,0,YLIM))
-	plt.legend(loc='best', prop={'size':8})
+fig, axes = plt.subplots(nrows=2, ncols=2)
 
-	if len(sys.argv) > 2:
-		attraction_same = []
-		attraction_other = []
+plt.subplot(221)
+plt.ylim((0,100))
+plt.title('Same subspecies')
+plt.ylabel('Type A', rotation=90, size='large')
+plt.plot(orientation_same, 'r', label="Orientation")
+plt.plot(attraction_same, 'k', label="Attraction")
+plt.legend(loc='best', prop={'size':8})
 
-		orientation_same = []
-		orientation_other = []
-		for step in f2_data:
-			attraction_same.append(step['Attraction']['s'])
-			attraction_other.append(step['Attraction']['o'])
+plt.subplot(222)
+plt.ylim((0,100))
+plt.title('Other subspecies')
+plt.plot(orientation_other, 'r', label="Orientation")
+plt.plot(attraction_other, 'k', label="Attraction")
+plt.legend(loc='best', prop={'size':8})
 
-			orientation_same.append(step['Orientation']['s'])
-			orientation_other.append(step['Orientation']['o'])
+attraction_same = []
+attraction_other = []
+orientation_same = []
+orientation_other = []
+for step in f2_data:
+	attraction_same.append(step['Attraction']['s'])
+	attraction_other.append(step['Attraction']['o'])
+	orientation_same.append(step['Orientation']['s'])
+	orientation_other.append(step['Orientation']['o'])
 
-		plt.subplot(223)
-		plt.title('same')
-		plt.plot(orientation_same, 'b', label="Orientation")
-		plt.plot(attraction_same, 'k', label="Attraction")
-		x1,x2,y1,y2 = plt.axis()
-		plt.axis((x1,x2,0,YLIM))
-		plt.legend(loc='best', prop={'size':8})
+plt.subplot(223)
+plt.ylim((0,100))
+plt.ylabel('Type B', rotation=90, size='large')
+plt.plot(orientation_same, 'r', label="Orientation")
+plt.plot(attraction_same, 'k', label="Attraction")
+plt.legend(loc='best', prop={'size':8})
 
-		plt.subplot(224)
-		plt.title('other')
-		plt.plot(orientation_other, 'b', label="Orientation")
-		plt.plot(attraction_other, 'k', label="Attraction")
-		x1,x2,y1,y2 = plt.axis()
-		plt.axis((x1,x2,0,YLIM))
-		plt.legend(loc='best', prop={'size':8})
-else:
-	plt.figure(1)
-	plt.subplot(221)
-	plt.title('attraction')
-	plt.plot(attraction_same, 'g', label='Same subspecies')
-	plt.plot(attraction_other, 'r', label='Other subspecies')
-	plt.legend(loc='best', prop={'size':8})
-	
-	plt.subplot(222)
-	plt.title('orientation')
-	plt.plot(orientation_same, 'g', label='Same subspecies')
-	plt.plot(orientation_other, 'r', label='Other subspecies')
-	plt.legend(loc='best', prop={'size':8})
+plt.subplot(224)
+plt.ylim((0,100))
+plt.plot(orientation_other, 'r', label="Orientation")
+plt.plot(attraction_other, 'k', label="Attraction")
+plt.legend(loc='best', prop={'size':8})
 
-	if len(sys.argv) > 2:
-		attraction_same = []
-		attraction_other = []
-
-		orientation_same = []
-		orientation_other = []
-		for step in f2_data:
-			attraction_same.append(step['Attraction']['s'])
-			attraction_other.append(step['Attraction']['o'])
-
-			orientation_same.append(step['Orientation']['s'])
-			orientation_other.append(step['Orientation']['o'])
-
-		plt.subplot(223)
-		plt.title('attraction')
-		plt.plot(attraction_same, 'g')
-		plt.plot(attraction_other, 'r')
-
-		plt.subplot(224)
-		plt.title('orientation')
-		plt.plot(orientation_same, 'g')
-		plt.plot(orientation_other, 'r')
-
+# fig.tight_layout()
 plt.show()
